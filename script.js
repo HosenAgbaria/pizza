e13081fbb81db0468e168aec0def444368fe1262
Hosen Agbaria <50136716+HosenAgbaria@users.noreply.github.com>
2021-01-10 20:44:01 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,171 +1,353 @@
-
-   //onChange call to calculate Total Price
-   let totalPrice = (e) => {
-    e.preventDefault();
-    myTotal();  //Total in Real-Time
-
-};
-
-
-
-// var TotalDiscount=0;
-// Submit
-function checkOrder(e) {
-  e.preventDefault();
-  try {
-    console.log("ee", e.target);
-    let olives=false;
-    let orderData = {
-      size: "",
-      toppings: [],
-      drinks: "",
-      fullName: "",
-      phone: "",
-      delivery: false,
-      address: "",
-      pickUpTime: "",
-      total: 0,
-    };
-    Object.values(e.target).forEach((element) => {
-      // orderData[element.name] = [Object.values(orderData[element.name]), element.value];
-      // if(orderData[e.target.name]){
-      //   orderData[e.target.name] = [...orderData[e.element.name],e.target.value]
-      // }else{
-      //   orderData[e.target.name] = [...orderData[e.element.name],e.target.value]
-      // }
-      if (element.name == "size" && element.checked) {
-        orderData.size = element.value;
-      } else if (element.name == "topping" && element.checked) {
-        orderData.toppings.push(element.value);
-        if(element.value=='olives')olives=true;
-      } else if (element.name == "drink" && element.checked) {
-        orderData.drinks = element.value;
-      } else if (element.name == "fullName") {
-        orderData.fullName = element.value;
-      } else if (element.name == "phone") {
-        orderData.phone = element.value;
-      } else if (element.name == "delivery" && element.checked) {
-        orderData.delivery = true;
-      } else if (element.name == "address") {
-        orderData.address = element.value;
-      } else if (element.name == "pickUpTime") {
-        orderData.pickUpTime = element.value;
-      } else if (element.name == "total") {
-        orderData.total = element.value;
-        console.log(orderData.total);
-      }
-
-      // console.log("elemnt", element);
-    });
-
-    console.log("orderData", orderData);
-  
-    orderData.total=document.getElementById("totalPrice").innerText;
-  // console.log(orderData);
-  // totalDiscount.apply(null,orderData);
-var TotalDiscount=0;
-console.log(olives);
-if(olives){
-  TotalDiscount = (orderData.total)*90/100;
-};
-
-console.log(TotalDiscount);
-
-
-
-
-} catch (error) {
-  console.error(error);
+.placeTitle {
+  display: flex;
+  justify-content: center;
+  font-size: 40px;
+  border: dotted 10px;
+  border-radius: 89px;
+  max-width: 800px;
+  margin: auto;
 }
+.logo {
+  z-index: -2;
 }
 
+.pizzaSize {
+  width: 280px;
+  margin-top: 80px;
+}
+.sizeOption {
+  width: fit-content;
+}
 
+.sizeOptions {
+  display: flex;
+  justify-content: space-around;
+  max-width: 450px;
+  margin: auto;
+}
+.sizeOp {
+  /* position: relative;
+  bottom: 55px;
+  font-weight: bold;
+  left: 4px; */
 
-let minTime = () => {
-  let t = document.getElementById("pickUpTime");
-  // t.max = "14:00";
-  t.min = "00:00";
-    try{
-  if (t.value > "11:00") {
-    t.style.background = "green";
-  } else {
-    t.style.background = "red";
-  }
-  console.log("cccccc", t);
-}catch(error) {
-    console.error(error);
+  position: relative;
+  bottom: 26px;
+  font-weight: bold;
+  right: 60px;
+  font-size: 25px;
+}
+/* .sizeContainer input[type="radio"] {
+  border: 0px;
+  width: 100%;
+  height: 50px;
+} */
+body input[type="radio"] {
+  /* border: 0px;
+  width: 100%;
+  height: 50px; */
+  border: 0px;
+  width: 80px;
+  height: 70px;
+}
+.price {
+  position: relative;
+  bottom: 10px;
+  color: #e91e63;
+  font-weight: blod;
+  font-weight: bold;
+  right: 9px;
+  bottom: -9px;
+}
+.drinkImage {
+  width: 70px;
+  height: 68px;
+  position: relative;
+  bottom: 74px;
+  right: 47px;
+}
+.fantaImg {
+  /* right: 25px; */
+  width: 60px;
+  height: 55px;
+  bottom: 78px;
+  right: 51px;
+  bottom: 68px;
+}
+.spriteImg {
+  width: 66px;
+  height: 62px;
+  position: relative;
+  right: 53px;
+  bottom: 72px;
+}
+.drinkName {
+  position: relative;
+  right: -40px;
+  bottom: 30px;
+}
+.drink {
+  width: fit-content;
+  position: relative;
+  flex: 0 50%;
+  /*demo*/
+  box-sizing: border-box;
+}
+.drinks {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+  max-width: 400px;
+  margin: auto;
+}
+.userDetails {
+  width: 90%;
+  max-width: 500px;
+  margin: auto;
+}
+.userDetailsInputs {
+  margin-bottom: 15px;
+  display: block;
+  margin: 20px auto;
+}
+.userAdress {
+  width: 90%;
+  max-width: 650px;
+}
+.orderTime {
+  width: 310px;
+  height: 50px;
+  text-align: center;
+}
+.totalPrice {
+  font-size: 31px;
+  display: block;
+  margin: 25px;
+  font-weight: bold;
+  border: solid;
+  width: fit-content;
+  margin: 35px auto;
+  padding: 25px;
+}
+.orderData {
+  display: none;
+  transition: visibility 0s, opacity 0.5s linear;
+}
+/* The container */
+.container {
+  display: block;
+  position: relative;
+  padding-left: 35px;
+  margin-bottom: 12px;
+  cursor: pointer;
+  font-size: 22px;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+  width: 344px;
+  text-align: start;
+  padding-left: 35px;
+}
 
-};
-};
+/* Hide the browser's default checkbox */
+.container input {
+  position: absolute;
+  opacity: 0;
+  cursor: pointer;
+  height: 0;
+  width: 0;
+}
 
-function myTotal(){
+.subTitle {
+  /* border: solid; */
+  width: fit-content;
+  padding: 20px;
+  margin-bottom: 20px auto;
+  /* display: flex; */
+  margin: 19px auto;
+  position: relative;
+  width: 380px;
+  text-align: initial;
+  max-width: 450px;
+}
+/* Create a custom checkbox */
+.checkmark {
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 25px;
+  width: 25px;
+  background-color: rgba(238, 238, 238, 0.863);
+}
+.toppings {
+  max-width: 500px;
+  text-align: start;
+  margin: auto;
+}
+.dORp {
+  position: relative;
+  top: 30px;
+  right: 77px;
+}
+.detailLabel {
+  position: relative;
+  right: 76px;
+  font-weight: bold;
+}
+/* On mouse-over, add a grey background color */
+.container:hover input ~ .checkmark {
+  background-color: rgba(204, 204, 204, 0);
+}
 
-  var globalTotalPrice = 0;
+/* When the checkbox is checked, add a blue background */
+.container input:checked ~ .checkmark {
+  background-color: #e91e63;
+}
 
+/* Create the checkmark/indicator (hidden when not checked) */
+.checkmark:after {
+  content: "";
+  position: absolute;
+  display: none;
+}
 
-  let myTotalToppings=[];
+/* Show the checkmark when checked */
+.container input:checked ~ .checkmark:after {
+  display: block;
+}
 
-  try{
-  const children = [...document.getElementsByTagName('input')];
-  children.forEach((child) => { 
-  
+/* Style the checkmark/indicator */
+.container .checkmark:after {
+  left: 9px;
+  top: 5px;
+  width: 5px;
+  height: 10px;
+  border: solid white;
+  border-width: 0 3px 3px 0;
+  -webkit-transform: rotate(45deg);
+  -ms-transform: rotate(45deg);
+  transform: rotate(45deg);
+}
 
-  
-  let orderData = {
-    size: "",
-    toppings: [],
-    drinks: "",
-    total: 0,
-  };
-  let prices = {
-    size: { small: 45, medium: 55, large: 65 },
-    topping: 3,
-    drinks: 10,
-  };
+/* //////////////////////////////// */
 
-  element = child;
-  if (element.name == "size" && element.checked) {
-    globalTotalPrice += prices.size[element.value];
-    console.log("total : ", globalTotalPrice);
-    orderData.size = element.value;
-    
+body {
+  font-family: Arial, Helvetica, sans-serif;
+}
+* {
+  box-sizing: border-box;
+}
 
-  } else if (element.name == "drink" && element.checked) {
-    if (element.value == "none") {
-      console.log(element.value);
-      // globalTotalPrice += 0;
-    } else {
-      console.log(prices.drinks);
-      console.log(element);
-       globalTotalPrice += prices.drinks;
- 
-      console.log("total : ", globalTotalPrice);
-      orderData.drinks = element.value;
-      console.log(element.value);
-    }
-  } else if (element.name == "topping" && element.checked) {
-    myTotalToppings.push(element.value);
+/* Button used to open the contact form - fixed at the bottom of the page */
+.open-button {
+  background-color: #555;
+  color: white;
+  padding: 16px 20px;
+  border: none;
+  cursor: pointer;
+  opacity: 0.8;
+  position: fixed;
+  bottom: 23px;
+  right: 28px;
+  width: 280px;
+}
 
-    if(myTotalToppings.length>3)
-    globalTotalPrice += 3;
-   
-    
+/* The popup form - hidden by default */
+.form-popup {
+  display: none;
+  position: fixed;
+  bottom: 0;
+  right: 15px;
+  border: 3px solid #f1f1f1;
+  z-index: 9;
+}
 
-  }
-  document.getElementById("totalPrice").innerText = globalTotalPrice;
-  
-  });
+/* Add styles to the form container */
+.form-container {
+  max-width: 300px;
+  padding: 10px;
+  background-color: white;
+}
 
+/* Full-width input fields */
+.form-container input[type="text"],
+.form-container input[type="password"] {
+  width: 100%;
+  padding: 15px;
+  margin: 5px 0 22px 0;
+  border: none;
+  background: #f1f1f1;
+}
 
-}catch(error) {
-    console.error(error);
+/* When the inputs get focus, do something */
+.form-container input[type="text"]:focus,
+.form-container input[type="password"]:focus {
+  background-color: #ddd;
+  outline: none;
+}
 
-};
-};
+/* Set a style for the submit/login button */
+.form-container .btn {
+  background-color: #4caf50;
+  color: white;
+  padding: 16px 20px;
+  border: none;
+  cursor: pointer;
+  width: 100%;
+  margin-bottom: 10px;
+  opacity: 0.8;
+}
 
+/* Add a red background color to the cancel button */
+.form-container .cancel {
+  background-color: red;
+}
 
-// Check if you got a discount because of choosing olives as one othe toppings
-// function totalDiscount(x){
-//   console.log(x);
+/* Add some hover effects to buttons */
+.form-container .btn:hover,
+.open-button:hover {
+  opacity: 1;
+}
+.orderData {
+  background-color: burlywood;
+  width: 80%;
+  margin: auto;
+  max-width: 600px;
+  border-radius: 30px;
+  /* Center vertically and horizontally */
+  position: absolute;
+  z-index: 1;
+  /* bottom: 150%; */
+  left: 15%;
+  margin: -25px 0 0 -25px; /* apply negative top and left margins to truly center the element */
+}
 
-// };
+.orderTotalPriceAfterDisc {
+  display: none;
+}
+.button {
+  background-color: #4caf50; /* Green */
+  border: none;
+  color: white;
+  padding: 16px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  transition-duration: 0.4s;
+  cursor: pointer;
+}
+
+.orderButton {
+  background-color: white;
+  color: black;
+  border-radius: 5px;
+  border: 2px solid #f44336;
+}
+
+.orderButton:hover {
+  background-color: #f44336;
+  color: white;
+}
